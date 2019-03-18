18103c1cc36da9472b742df9db4713a68b5619e8
Francisco Manzano <fmanzano@qdqmedia.com>
2019-03-18 15:01:21 +0100

    json marshal tests

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Image struct {
+	Name string
+}
+
+type Data struct {
+	Images []Image
+}
+
+func serializeJSON(data Data, response http.ResponseWriter, request *http.Request) {
+	js, err := json.Marshal(data)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response.Header().Set("Content-Type", "application/json")
+	response.Write(js)
+}
+
 func indexView(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "Hello")
+	data := Data{[]Image{Image{"docker"}, Image{"mongodb"}}}
+	serializeJSON(data, response, request)
 }
 
 func main() {
