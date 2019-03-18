851db5584d523760225d2dc0e3aea4d3cd763c9c
Francisco Manzano <fmanzano@qdqmedia.com>
2019-03-18 12:46:58 +0100

    Initial server

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexView(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Hello")
+}
 
 func main() {
-	fmt.Println("Hola")
+	http.HandleFunc("/", indexView)
+	fmt.Println("Serving on port 12001")
+	http.ListenAndServe(":12001", nil)
 }
