2366c258338fc32a84ff4633cada116c982e7328
Francisco Manzano <fmanzano@qdqmedia.com>
2019-03-27 17:20:46 +0100

    Containers endpoint with running containers

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"context"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 	"github.com/savsgio/atreugo/v7"
 )
 
@@ -13,12 +17,25 @@ func main() {
 
 	server := atreugo.New(config)
 
-	server.Path("GET", "/", func(ctx *atreugo.RequestCtx) error {
-		return ctx.JSONResponse(atreugo.JSON{"Daemon": "Ok"})
-	})
-
-	err := server.ListenAndServe()
+	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
+	}
+
+	server.Path("GET", "/", func(ctx *atreugo.RequestCtx) error {
+		return ctx.JSONResponse(atreugo.JSON{"status": "Ok"})
+	})
+
+	server.Path("GET", "/containers", func(ctx *atreugo.RequestCtx) error {
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+		if err != nil {
+			panic(err)
+		}
+		return ctx.JSONResponse(atreugo.JSON{"containers": containers, "status": "Ok"})
+	})
+
+	servError := server.ListenAndServe()
+	if servError != nil {
+		panic(servError)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/orchestrapi/orchestra-daemon
 go 1.12
 
 require (
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.3.3 // indirect
+	github.com/opencontainers/go-digest v1.0.0-rc1 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/savsgio/atreugo/v7 v7.1.1
 	github.com/valyala/fasthttp v1.2.0
 )
